a6126234f146073962f3d7809aa38168602d528c
Smurvy <riley@motylinski.com>
2024-01-26 21:41:26 -0500

    Day 10
    Accidentally yesterdays commit as day 8 instead of 9; time is flying fast! I figured out the chart stuff and have it centered. My next goals are hooking up the website to a database instead of feeding the valuesinto python. My time with chart.js has been tough so far as I struggled adding the actual chart into the website, but I've learned alot more after reading through the api docs. I feel like even if I don't retain a lot of the info I absorb through the time I spend on these projects, I am at least learning what it feels like to struggle through and understand a library you have never used before. I'm honestly quite impressed in my ability to understnad the library and javascript as a whole

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def homepage():
         ("03-01-2020",1386),
         ("04-01-2020",1087),
         ("05-01-2020",1698),
-        ("06-01-2020",987),
+        ("06-01-2020",1987)
     ]
     
     labels = [row[0] for row in data]
     values = [row[1] for row in data]
 
-    return render_template('graph.html')
+    return render_template('graph.html', labels=labels, values=values)
